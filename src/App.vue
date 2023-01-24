bed4300d6b795ee244219ce8bebcb944a36130f0
usamahamid <usamahamid13@gmail.com>
2023-01-24 21:08:25 +0500

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+  color: #2c3e50;
+}
+#app,
+.main,
+.inner,
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.main,
+.nested-inner {
+  background: #eb8e5f;
+}
+.main,
+.inner,
+.nested-inner {
+  border-radius: 50%;
+}
+.inner,
+.nested-inner {
+  width: 95%;
+  height: 95%;
+  display: flex;
+  justify-content: center;
+}
+.main {
+  width: 40vw;
+  height: 40vw;
+}
+
+.inner {
+  background: white;
+}
+.nested-inner {
+  padding: 1%;
+}
+.wrapper {
+  padding: 1%;
+}
+
+.content {
+  flex-direction: column;
+  padding: 0 8%;
+  justify-content: space-between;
+  height: 85%;
+}
+.content .drag {
+  height: 100%;
+  justify-content: space-between;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+.content .drag div p {
+  text-align: center;
+  color: white;
+  font-weight: 600;
+  font-size: 2vw;
+}
+.content .drag div input {
+  position: relative;
+  background: white;
+  border: none;
+  color: #2c3e50;
+  border-radius: 5px;
+  width: 25vw;
+  height: 6vw;
+  font-size: 2vw;
+}
+.content .drag div button {
+  background: #2c3e50;
+  border: none;
+  color: white;
+  border-radius: 5px;
+  width: 50%;
+  width: 15vw;
+  height: 6vw;
+  font-size: 2vw;
+  text-transform: uppercase;
+}
+</style>
---
+++ src/views/PopUp.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="">
+    <div class="main">
+      <div class="inner">
+        <div class="nested-inner">
+          <div class="wrapper">
+            <div class="content">
+              <draggable
+                v-model="myArray"
+                @start="drag = true"
+                @end="drag = false"
+                class="drag"
+              >
+                <div v-for="element in myArray" :key="element.id">
+                  <IconStars v-if="element === 1" />
+                  <p contenteditable="true" v-if="element === 2">
+                    All the text and elements in this popup should be editable
+                    and draggable
+                  </p>
+
+                  <input
+                    class="input"
+                    type="text"
+                    placeholder="E-mail"
+                    v-if="element === 3"
+                  />
+                  <button
+                    contenteditable="true"
+                    class="button"
+                    v-if="element === 4"
+                  >
+                    signup now
+                  </button>
+                </div>
+              </draggable>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import IconStars from "@/components/IconStars.vue";
+import draggable from "vuedraggable";
+
+export default {
+  components: { IconStars, draggable },
+  data() {
+    return {
+      myArray: [1, 2, 3, 4],
+    };
+  },
+};
+</script>
